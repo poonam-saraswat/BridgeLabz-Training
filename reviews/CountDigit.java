b4b6aed8bbe1f694ba012e305ecd845d1d67846c
Poonam Saraswat <poonamsaraswat3105@gmail.com>
2025-12-23 15:27:18 +0530

    [Poonam Saraswat] Added solution of the review problem

---
+++ reviews/CountDigit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CountDigit{
+   public static void main(String [] args){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number");
+       int num = sc.nextInt();
+       
+       //Counnt digits
+       int count = 0;              
+       while(num != 0){
+           num = num / 10;  
+           count++;      
+       }
+       System.out.println("Number of digits: " + count);
+}
+}
