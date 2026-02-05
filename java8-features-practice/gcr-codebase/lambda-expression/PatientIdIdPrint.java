609fa16e643fafce7f45ef8c410c4f7f2860e258
Poonam Saraswat <poonamsaraswat3105@gmail.com>
2026-02-05 20:12:11 +0530

    Updated changes

--- java8-features-practice/gcr-codebase/lambda-expression/PatientIdIdPrint.java
+++ java8-features-practice/gcr-codebase/lambda-expression/PatientIdIdPrint.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class PatientIdPrint {
+public class PatientIdIdPrint {
     public static void main(String[] args) {
         List<Integer> patientIds = List.of(101, 102, 103);
         patientIds.forEach(System.out::println);
